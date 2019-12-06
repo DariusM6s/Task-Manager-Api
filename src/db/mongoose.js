b3546bf251f927b00491d6f305fa0ea814c31996
Darius Maciunas <dmaciunas@outlook.com>
2019-12-06 20:30:44 +0000

    tryin to fix BS

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-require('dotenv').config({ path: './config/dev.env' });
+// require('dotenv').config({ path: './config/dev.env' });
 
 mongoose.connect(process.env.MONGODB_URL, {
 	useNewUrlParser    : true,
 	useCreateIndex     : true,
+	useFindAndModify   : false,
 	useUnifiedTopology : true
 });
